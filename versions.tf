d38bc004c1fc7920cf33bbd600278e6692570821
Ubuntu <azarjaved@Ubuntu.tp5hdd5hmj5ujdjgxbztiy1jwc.gvxx.internal.cloudapp.net>
2024-02-18 21:42:43 +0000

    add provider back

--- versions.tf
+++ versions.tf
@@ -1,12 +1,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.56.0, < 4.0.0"
+      configuration_aliases = [azurerm.main]
     }
   }
-}
-
-provider "azurerm" {
-  alias = "main"
-  features {}
+  required_version = ">= 1.4.0"
 }
