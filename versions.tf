456b6950961027d7e62f7857686acdb8687ed721
Ubuntu <azarjaved@Ubuntu.tp5hdd5hmj5ujdjgxbztiy1jwc.gvxx.internal.cloudapp.net>
2024-02-18 21:05:12 +0000

    add provider

--- versions.tf
+++ versions.tf
@@ -1,12 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.90.0"
+      source = "hashicorp/azurerm"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
